dea8e0f62045278a972b6e3304cf25ee1675f629
thiirteen <truehmer@gmail.com>
2017-10-05 17:01:53 -0500

    Added Sass Framework

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -7,7 +7,7 @@ class Main extends React.Component {
 	render() {
 		return (
 			<div className={"TR-Main"}>
-				Main Area
+				Main Area <span className={"TR-Icon"}>&#xf0f9;</span>
 			</div>
 		)
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,18 @@ var config = {
                 test: /\.(s?)css$/,
                 include: SRC_DIR,
                 loader: "style-loader!css-loader!sass-loader"
-            }
+            },
+            {
+        		test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        		use: [{
+        		  loader: 'file-loader',
+        		  options: {
+        		    name: '[name].[ext]',
+        		    outputPath: 'fonts/',    // where the fonts will go
+        		    publicPath: '../'       // override the default path
+        		  }
+        		}]
+       		}	
         ]
     }
 };
